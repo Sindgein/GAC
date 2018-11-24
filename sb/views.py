fa743d095a5e5016ce4f913ee2479f8352b2c943
How <Fuermohao@outlook.com>
2018-11-24 18:49:04 +0800

    added: login required

--- sb/views.py
+++ sb/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required
 def game1(request):
     return render(request,'game1sb.html')
 
+@login_required
 def game2(request):
     return render(request,'game2sb.html')
